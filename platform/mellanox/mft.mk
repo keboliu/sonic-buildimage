63604ac7f802d2e8f6132a6f67ecd1b65ce92452
Kebo Liu <kebol@nvidia.com>
2023-03-10 19:41:19 +0800

    [202211_SPC4_bringup] update MFT to 4.23.0-307

--- platform/mellanox/mft.mk
+++ platform/mellanox/mft.mk
@@ -17,9 +17,9 @@
 # Mellanox SAI
 
 MFT_VERSION = 4.23.0
-MFT_REVISION = 305
+MFT_REVISION = 307
 
-MLNX_MFT_INTERNAL_SOURCE_BASE_URL = http://nbu-nfs.mellanox.com/auto/mswg/release/mft/mft-4.23.0/mft-4.23.0-305/linux/mft-4.23.0-305/DEBS/
+MLNX_MFT_INTERNAL_SOURCE_BASE_URL = http://nbu-nfs.mellanox.com/auto/mswg/release/mft/mft-4.23.0/mft-4.23.0-307/linux/mft-4.23.0-307/DEBS/
 
 ifneq ($(MLNX_MFT_INTERNAL_SOURCE_BASE_URL), )
 MFT_FROM_INTERNAL = y
