040fe1c9fbc553f2efdbc1ee80f5f7d49fb842d1
david.zagury <davidza@nvidia.com>
2023-04-03 19:18:35 +0300

    [202211_SPC4_ES] Temp fix for prod secure 6

--- scripts/sign_image_x86_64-mellanox-r0.sh
+++ scripts/sign_image_x86_64-mellanox-r0.sh
@@ -35,7 +35,7 @@ sign_image_prod()
 
   SERVER_SIGN_SCRIPT=/opt/nvidia/sonic_sign.sh
   # signing with prod server
-  ${SERVER_SIGN_SCRIPT} --sandbox ${HOST_PATH}
+  ${SERVER_SIGN_SCRIPT} --sandbox ${HOST_PATH} \
                       --file ${UNSIGNED_IMG} \
                       --type CMS --prod \
                       --description 'CMS Signing NVOS IMG' \
