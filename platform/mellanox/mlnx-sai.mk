57bb9ee516639e2d57a04ec0df52e99cab8e4d03
Kebo Liu <kebol@nvidia.com>
2023-06-09 13:49:32 +0800

    update SAI make file
    Signed-off-by: Kebo Liu <kebol@nvidia.com>

--- platform/mellanox/mlnx-sai.mk
+++ platform/mellanox/mlnx-sai.mk
@@ -1,18 +1,13 @@
 # Mellanox SAI
 
-#MLNX_SAI_VERSION = SAIBuild2305.24.0.1
-MLNX_SAI_VERSION = SAIBuild2211.25.0.0
-
-# mlnx-sai_1.mlnx.SAIBuild2305.24.0.1_amd64.deb
-# mlnx-sai-dbgsym_1.mlnx.SAIBuild2305.24.0.1_amd64.deb
-
+MLNX_SAI_VERSION = SAIBuild2305.24.0.1
 MLNX_SAI_ASSETS_GITHUB_URL = https://github.com/Mellanox/Spectrum-SDK-Drivers-SONiC-Bins
 MLNX_SAI_ASSETS_RELEASE_TAG = sai-$(MLNX_SAI_VERSION)-$(BLDENV)-$(CONFIGURED_ARCH)
 MLNX_SAI_ASSETS_URL = $(MLNX_ASSETS_GITHUB_URL)/releases/download/$(MLNX_SAI_ASSETS_RELEASE_TAG)
 MLNX_SAI_DEB_VERSION = $(subst -,.,$(subst _,.,$(MLNX_SAI_VERSION)))
 
 # Place here URL where SDK sources exist
-MLNX_SAI_SOURCE_BASE_URL = http://nbu-nfs.mellanox.com/auto/sw_system_release/sonic/kebol/SAI_test/
+MLNX_SAI_SOURCE_BASE_URL = 
 
 ifneq ($(MLNX_SAI_SOURCE_BASE_URL), )
 SAI_FROM_SRC = y
@@ -29,6 +24,14 @@ $(MLNX_SAI)_RDEPENDS += $(MLNX_SDK_RDEBS) $(MLNX_SDK_DEBS)
 $(eval $(call add_conflict_package,$(MLNX_SAI),$(LIBSAIVS_DEV)))
 MLNX_SAI_DBGSYM = mlnx-sai-dbgsym_1.mlnx.$(MLNX_SAI_VERSION)_$(CONFIGURED_ARCH).deb
 $(eval $(call add_derived_package,$(MLNX_SAI),$(MLNX_SAI_DBGSYM)))
+
+define make_url
+	$(1)_URL = $(MLNX_SAI_ASSETS_URL)/$(1)
+
+endef
+
+$(eval $(foreach deb,$(MLNX_SAI) $(MLNX_SAI_DBGSYM),$(call make_url,$(deb))))
+
 ifeq ($(SAI_FROM_SRC), y)
 SONIC_MAKE_DEBS += $(MLNX_SAI)
 else
